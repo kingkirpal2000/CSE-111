37067c62e5fe3be0379bf4c8999adceb7ae37b69
Sabir Kirpal <skirpal2000@gmail.com>
2021-10-29 23:20:39 -0700

    Finished as much as I possibly can

--- Midterm/midterm.sql
+++ Midterm/midterm.sql
@@ -92,8 +92,11 @@ GROUP BY Classes.country;
 SELECT "4----------";
 .headers on
 --put your code here
-
-;
+INSERT INTO Outcomes(ship, battle, result)
+SELECT s.name, "Denmark Strait", "damaged"
+FROM Ships as s
+WHERE NOT(s.name IN (SELECT ship from Outcomes WHERE Outcomes.battle = "Denmark Strait"))
+AND s.launched <= 1920;
 .headers off
 
 SELECT "5----------";
@@ -110,15 +113,24 @@ GROUP BY Classes.country;
 SELECT "6----------";
 .headers on
 --put your code here
-SELECT sq1.cntry, MIN(sq1.cnt)
+SELECT sq1.cntry, sq1.cnt
 FROM (
     SELECT Classes.country as cntry, COUNT(Outcomes.result) as cnt
     FROM Outcomes, Classes, Ships
     WHERE Classes.class = Ships.class
     AND Ships.name = Outcomes.ship
-    AND Outcomes.result != "damaged"
+    AND Outcomes.result = "damaged"
     GROUP BY Classes.country
-) sq1;
+) sq1
+WHERE sq1.cnt = (SELECT MIN(sq2.cnt) FROM (
+    SELECT Classes.country as cntry, COUNT(Outcomes.result) as cnt
+    FROM Outcomes, Classes, Ships
+    WHERE Classes.class = Ships.class
+    AND Ships.name = Outcomes.ship
+    AND Outcomes.result = "damaged"
+    GROUP BY Classes.country
+) sq2)
+GROUP BY sq1.cntry;
 .headers off
 
 SELECT "7----------";
@@ -143,43 +155,77 @@ WHERE ship IN(
 SELECT "8----------";
 .headers on
 --put your code here
-;
+SELECT DISTINCT Outcomes.ship
+FROM outcomes, ships, classes
+WHERE Ships.class = Classes.class
+AND Ships.name = Outcomes.ship
+and Outcomes.result = "damaged"
+GROUP BY Outcomes.ship
+HAVING COUNT(Outcomes.result = "damaged") > 1;
 .headers off
 
 SELECT "9----------";
 .headers on
 --put your code here
-;
 .headers off
 
 SELECT "10---------";
 .headers on
 --put your code here
-;
+UPDATE Classes SET numGuns = numGuns * 2
+WHERE Classes.class IN(
+    SELECT Classes.class
+    FROM Classes, Ships
+    WHERE Classes.class = Ships.class
+    AND Ships.launched >= 1940
+);
 .headers off
 
 SELECT "11---------";
 .headers on
 --put your code here
-;
+SELECT Classes.class
+FROM Ships, Classes
+WHERE Ships.class = Classes.class
+GROUP BY Classes.class
+HAVING COUNT(Classes.class) = 2;
 .headers off
 
 SELECT "12---------";
 .headers on
 --put your code here
-;
+SELECT Classes.class, COUNT(*)
+FROM Classes, Ships, Outcomes
+WHERE Outcomes.ship = Ships.name
+AND Classes.country IN (
+    SELECT Classes.country
+    FROM Ships, Classes, Outcomes
+    WHERE Outcomes.result = "damaged"
+)
+GROUP BY Classes.country
+ORDER BY Count(*)
+LIMIT 1;
+
 .headers off
 
 SELECT "13---------";
 .headers on
 --put your code here
-;
+DELETE FROM Ships
+WHERE name IN (
+    SELECT ship
+    FROM Outcomes
+    WHERE result = "sunk"
+);
 .headers off
 
 SELECT "14---------";
 .headers on
 --put your code here
-;
+SELECT Classes.Country, TOTAL(numGuns)
+FROM Ships, Classes
+WHERE Ships.class = Classes.class
+GROUP BY Classes.country;
 .headers off
 
 SELECT "15---------";
@@ -191,7 +237,12 @@ SELECT "15---------";
 SELECT "16---------";
 .headers on
 --put your code here
-;
+INSERT INTO Ships(name, class, launched)
+SELECT Classes.class, Classes.class, Ships.launched -- name of ship is same as class name
+FROM Ships, Classes
+WHERE Classes.class NOT IN (SELECT Ships.class FROM Ships)
+AND Ships.launched = (SELECT min(Ships.launched) FROM Ships, Classes WHERE Ships.class = Classes.class)
+GROUP BY Classes.class;
 .headers off
 
 SELECT "17---------";
